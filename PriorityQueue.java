4dedd061ad23908480b498155618d9a53bcee993
TakingTheL <yliu6@stuy.edu>
2017-04-20 10:08:28 -0400

    added PriorityQueue interface

---
+++ PriorityQueue.java
@@ -0,0 +1,6 @@
+public interface PriorityQueue{
+    public boolean isEmpty();
+    public void add(java.lang.Object x);
+    public java.lang.Object removeMin();
+    public java.lang.Object peekMin();
+}
