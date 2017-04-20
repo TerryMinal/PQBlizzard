cd574ad133e976357660e6a90551c0c33a2d0924
Lisa Eng <leng3@stuy.edu>
2017-04-20 10:11:12 -0400

    Ticket.java upload

---
+++ Ticket.java
@@ -0,0 +1,13 @@
+public class Ticket implements Comparable {
+
+    private int status;
+    private int priority;
+    private String name;
+    private String descrip;
+    private int ID;
+
+    public Ticket(){
+	
+    }
+
+}
