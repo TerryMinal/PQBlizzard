193371b45c4714d581786a848ffe3445460963ec
TakingTheL <yliu6@stuy.edu>
2017-04-21 09:55:34 -0400

    Started on HelpDesk.java

---
+++ HelpDesk.java
@@ -0,0 +1,9 @@
+public class HelpDesk{
+    public static void main(String[] args){
+	System.out.println("Welcome to our help desk!");
+	System.out.print("Please enter your name: ");
+	String name = Keyboard.readString();
+	System.out.println("\nWhat is the problem? ");
+	
+    }
+}
