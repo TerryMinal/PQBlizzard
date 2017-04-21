5c500834b25d1c0368c76a1b30dbeed8bf1b6735
TakingTheL <yliu6@stuy.edu>
2017-04-21 10:16:04 -0400

    on the path to glory

--- HelpDesk.java
+++ HelpDesk.java
@@ -1,9 +1,14 @@
 public class HelpDesk{
     public static void main(String[] args){
 	System.out.println("Welcome to our help desk!");
-	System.out.print("Please enter your name: ");
+	System.out.println("Please enter your name: ");
 	String name = Keyboard.readString();
 	System.out.println("\nWhat is the problem? ");
+	String descrip = Keyboard.readString();
+	System.out.println("");
+
+	//get int counter for id number from APQ
+	// build ticket and add to APQ
 	
     }
 }
